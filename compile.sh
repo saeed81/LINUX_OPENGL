6a4d4426e712a034ad14e321ff223277397ede69
saeed81 <sd.falahat@gmail.com>
2021-05-08 09:43:23 +0200

    a bit more info

--- compile.sh
+++ compile.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 #gcc -o quad quad.c -lX11 -lGL -lGLU
 gcc -o main.exe cube.c -lX11 -lGL -lGLU
+#sudo apt-get install libglew-dev libsdl2-dev libsdl2-image-dev libglm-dev libfreetype6-dev
+#sudo apt-get install build-essential libgl1-mesa-dev
